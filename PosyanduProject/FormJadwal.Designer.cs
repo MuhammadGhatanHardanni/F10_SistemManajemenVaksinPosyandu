c35bad0b549a9b5be47811f72f91b72ba5f4cd39
Ghatan55 <ghatanhardanni@gmail.com>
2026-04-19 17:03:22 +0700

    Mendesain UI penjadwalan kegiatan Posyandu

---
+++ PosyanduProject/FormJadwal.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace PosyanduProject
+{
+    partial class FormJadwal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdJadwal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpPelaksanaan = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLokasi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKeterangan = new System.Windows.Forms.TextBox();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnBersih = new System.Windows.Forms.Button();
+            this.txtCari = new System.Windows.Forms.TextBox();
+            this.btnCari = new System.Windows.Forms.Button();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.dgvJadwal = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJadwal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Jadwal";
+            // 
+            // txtIdJadwal
+            // 
+            this.txtIdJadwal.Location = new System.Drawing.Point(15, 28);
+            this.txtIdJadwal.Name = "txtIdJadwal";
+            this.txtIdJadwal.ReadOnly = true;
+            this.txtIdJadwal.Size = new System.Drawing.Size(63, 22);
+            this.txtIdJadwal.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tanggal Pelaksanaan";
+            // 
+            // dtpPelaksanaan
+            // 
+            this.dtpPelaksanaan.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpPelaksanaan.Location = new System.Drawing.Point(18, 91);
+            this.dtpPelaksanaan.Name = "dtpPelaksanaan";
+            this.dtpPelaksanaan.Size = new System.Drawing.Size(138, 22);
+            this.dtpPelaksanaan.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Lokasi";
+            // 
+            // txtLokasi
+            // 
+            this.txtLokasi.Location = new System.Drawing.Point(21, 150);
+            this.txtLokasi.Name = "txtLokasi";
+            this.txtLokasi.Size = new System.Drawing.Size(135, 22);
+            this.txtLokasi.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Keterangan";
+            // 
+            // txtKeterangan
+            // 
+            this.txtKeterangan.Location = new System.Drawing.Point(21, 213);
+            this.txtKeterangan.Multiline = true;
+            this.txtKeterangan.Name = "txtKeterangan";
+            this.txtKeterangan.Size = new System.Drawing.Size(135, 77);
+            this.txtKeterangan.TabIndex = 7;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.BackColor = System.Drawing.Color.Green;
+            this.btnTambah.ForeColor = System.Drawing.Color.White;
+            this.btnTambah.Location = new System.Drawing.Point(21, 296);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(75, 35);
+            this.btnTambah.TabIndex = 8;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = false;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Blue;
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(142, 296);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 36);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.BackColor = System.Drawing.Color.Red;
+            this.btnHapus.ForeColor = System.Drawing.Color.White;
+            this.btnHapus.Location = new System.Drawing.Point(21, 367);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 35);
+            this.btnHapus.TabIndex = 10;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = false;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnBersih
+            // 
+            this.btnBersih.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnBersih.Location = new System.Drawing.Point(142, 367);
+            this.btnBersih.Name = "btnBersih";
+            this.btnBersih.Size = new System.Drawing.Size(75, 35);
+            this.btnBersih.TabIndex = 11;
+            this.btnBersih.Text = "Bersih";
+            this.btnBersih.UseVisualStyleBackColor = false;
+            this.btnBersih.Click += new System.EventHandler(this.btnBersih_Click);
+            // 
+            // txtCari
+            // 
+            this.txtCari.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCari.Location = new System.Drawing.Point(368, 32);
+            this.txtCari.Name = "txtCari";
+            this.txtCari.Size = new System.Drawing.Size(190, 22);
+            this.txtCari.TabIndex = 12;
+            this.txtCari.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCari_KeyDown);
+            // 
+            // btnCari
+            // 
+            this.btnCari.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCari.Location = new System.Drawing.Point(564, 32);
+            this.btnCari.Name = "btnCari";
+            this.btnCari.Size = new System.Drawing.Size(75, 23);
+            this.btnCari.TabIndex = 13;
+            this.btnCari.Text = "Cari";
+            this.btnCari.UseVisualStyleBackColor = true;
+            this.btnCari.Click += new System.EventHandler(this.btnCari_Click);
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTampilkan.Location = new System.Drawing.Point(645, 32);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(111, 23);
+            this.btnTampilkan.TabIndex = 14;
+            this.btnTampilkan.Text = "Tampilkan Data";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            this.btnTampilkan.Click += new System.EventHandler(this.btnTampilkan_Click);
+            // 
+            // dgvJadwal
+            // 
+            this.dgvJadwal.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvJadwal.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvJadwal.BackgroundColor = System.Drawing.Color.White;
+            this.dgvJadwal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJadwal.Location = new System.Drawing.Point(368, 60);
+            this.dgvJadwal.Name = "dgvJadwal";
+            this.dgvJadwal.RowHeadersWidth = 51;
+            this.dgvJadwal.RowTemplate.Height = 24;
+            this.dgvJadwal.Size = new System.Drawing.Size(388, 342);
+            this.dgvJadwal.TabIndex = 15;
+            this.dgvJadwal.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvJadwal_CellClick);
+            // 
+            // FormJadwal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvJadwal);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.btnCari);
+            this.Controls.Add(this.txtCari);
+            this.Controls.Add(this.btnBersih);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.txtKeterangan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLokasi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpPelaksanaan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdJadwal);
+            this.Controls.Add(this.label1);
+            this.Name = "FormJadwal";
+            this.Text = "FormJadwal";
+            this.Load += new System.EventHandler(this.FormJadwal_Load);
+            this.Click += new System.EventHandler(this.btnTambah_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJadwal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdJadwal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpPelaksanaan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLokasi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKeterangan;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnBersih;
+        private System.Windows.Forms.TextBox txtCari;
+        private System.Windows.Forms.Button btnCari;
+        private System.Windows.Forms.Button btnTampilkan;
+        private System.Windows.Forms.DataGridView dgvJadwal;
+    }
+}
