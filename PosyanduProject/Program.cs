067eca375d74d8d7a033c757e75e603413c33dfa
Ghatan55 <ghatanhardanni@gmail.com>
2026-04-19 16:50:18 +0700

    Mengatur halaman utama aplikasi

---
+++ PosyanduProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PosyanduProject
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormSplash());
+        }
+    }
+}
