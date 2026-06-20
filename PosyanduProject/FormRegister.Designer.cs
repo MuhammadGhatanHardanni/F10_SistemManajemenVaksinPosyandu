c4a2de37110fa570c307ced291782a106414dfcf
Ghatan55 <ghatanhardanni@gmail.com>
2026-06-20 19:16:05 +0700

    Redesain UI form Register

--- PosyanduProject/FormRegister.Designer.cs
+++ PosyanduProject/FormRegister.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace PosyanduProject
+namespace PosyanduProject
 {
     partial class FormRegister
     {
@@ -45,90 +45,123 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(216, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(116)))), ((int)(((byte)(144)))));
+            this.label1.Location = new System.Drawing.Point(80, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(326, 32);
+            this.label1.Size = new System.Drawing.Size(290, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Daftar Akun Orang Tua";
+            this.label1.Text = "Daftar Akun Posyandu";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(259, 110);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(75, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.Size = new System.Drawing.Size(113, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nama Lengkap";
             // 
             // txtNama
             // 
-            this.txtNama.Location = new System.Drawing.Point(401, 103);
+            this.txtNama.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNama.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtNama.Location = new System.Drawing.Point(75, 135);
             this.txtNama.Name = "txtNama";
-            this.txtNama.Size = new System.Drawing.Size(100, 22);
+            this.txtNama.Size = new System.Drawing.Size(300, 32);
             this.txtNama.TabIndex = 2;
             this.txtNama.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtNama_KeyDown);
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.Location = new System.Drawing.Point(401, 151);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(100, 22);
-            this.txtUsername.TabIndex = 3;
-            this.txtUsername.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUsername_KeyDown);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(259, 157);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(75, 185);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.Size = new System.Drawing.Size(80, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtUsername.Location = new System.Drawing.Point(75, 210);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(300, 32);
+            this.txtUsername.TabIndex = 3;
+            this.txtUsername.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUsername_KeyDown);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(259, 201);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(75, 260);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.Size = new System.Drawing.Size(76, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Password";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(401, 195);
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtPassword.Location = new System.Drawing.Point(75, 285);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.Size = new System.Drawing.Size(300, 32);
             this.txtPassword.TabIndex = 6;
             this.txtPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPassword_KeyDown);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(259, 245);
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(75, 335);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(132, 16);
+            this.label5.Size = new System.Drawing.Size(155, 20);
             this.label5.TabIndex = 7;
             this.label5.Text = "Konfirmasi Password";
             // 
             // txtKonfirmasi
             // 
-            this.txtKonfirmasi.Location = new System.Drawing.Point(401, 239);
+            this.txtKonfirmasi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtKonfirmasi.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtKonfirmasi.Location = new System.Drawing.Point(75, 360);
             this.txtKonfirmasi.Name = "txtKonfirmasi";
             this.txtKonfirmasi.PasswordChar = '*';
-            this.txtKonfirmasi.Size = new System.Drawing.Size(100, 22);
+            this.txtKonfirmasi.Size = new System.Drawing.Size(300, 32);
             this.txtKonfirmasi.TabIndex = 8;
             this.txtKonfirmasi.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtKonfirmasi_KeyDown);
             // 
+            // chkShowPassword
+            // 
+            this.chkShowPassword.AutoSize = true;
+            this.chkShowPassword.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.chkShowPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.chkShowPassword.Location = new System.Drawing.Point(75, 400);
+            this.chkShowPassword.Name = "chkShowPassword";
+            this.chkShowPassword.Size = new System.Drawing.Size(161, 24);
+            this.chkShowPassword.TabIndex = 11;
+            this.chkShowPassword.Text = "Tampilkan Password";
+            this.chkShowPassword.UseVisualStyleBackColor = true;
+            this.chkShowPassword.CheckedChanged += new System.EventHandler(this.chkShowPassword_CheckedChanged);
+            // 
             // btnDaftar
             // 
-            this.btnDaftar.BackColor = System.Drawing.Color.Green;
+            this.btnDaftar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(116)))), ((int)(((byte)(144)))));
+            this.btnDaftar.FlatAppearance.BorderSize = 0;
+            this.btnDaftar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDaftar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.btnDaftar.ForeColor = System.Drawing.Color.White;
-            this.btnDaftar.Location = new System.Drawing.Point(262, 287);
+            this.btnDaftar.Location = new System.Drawing.Point(75, 440);
             this.btnDaftar.Name = "btnDaftar";
-            this.btnDaftar.Size = new System.Drawing.Size(114, 34);
+            this.btnDaftar.Size = new System.Drawing.Size(300, 45);
             this.btnDaftar.TabIndex = 9;
             this.btnDaftar.Text = "Daftar Sekarang";
             this.btnDaftar.UseVisualStyleBackColor = false;
@@ -136,32 +169,25 @@
             // 
             // btnBatal
             // 
-            this.btnBatal.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.btnBatal.ForeColor = System.Drawing.Color.White;
-            this.btnBatal.Location = new System.Drawing.Point(425, 286);
+            this.btnBatal.BackColor = System.Drawing.Color.White;
+            this.btnBatal.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.btnBatal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBatal.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnBatal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(116)))), ((int)(((byte)(139)))));
+            this.btnBatal.Location = new System.Drawing.Point(75, 495);
             this.btnBatal.Name = "btnBatal";
-            this.btnBatal.Size = new System.Drawing.Size(75, 35);
+            this.btnBatal.Size = new System.Drawing.Size(300, 40);
             this.btnBatal.TabIndex = 10;
-            this.btnBatal.Text = "Batal";
+            this.btnBatal.Text = "Kembali ke Login";
             this.btnBatal.UseVisualStyleBackColor = false;
             this.btnBatal.Click += new System.EventHandler(this.btnBatal_Click);
-            // 
-            // chkShowPassword
-            // 
-            this.chkShowPassword.AutoSize = true;
-            this.chkShowPassword.Location = new System.Drawing.Point(401, 267);
-            this.chkShowPassword.Name = "chkShowPassword";
-            this.chkShowPassword.Size = new System.Drawing.Size(156, 20);
-            this.chkShowPassword.TabIndex = 11;
-            this.chkShowPassword.Text = "Tampilkan Password";
-            this.chkShowPassword.UseVisualStyleBackColor = true;
-            this.chkShowPassword.CheckedChanged += new System.EventHandler(this.chkShowPassword_CheckedChanged);
             // 
             // FormRegister
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(450, 580);
             this.Controls.Add(this.chkShowPassword);
             this.Controls.Add(this.btnBatal);
             this.Controls.Add(this.btnDaftar);
@@ -169,13 +195,16 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtNama);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "FormRegister";
-            this.Text = "FormRegister";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Register Akun";
             this.Load += new System.EventHandler(this.FormRegister_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
